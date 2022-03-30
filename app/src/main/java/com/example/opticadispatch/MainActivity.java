046805cb18dd9cd3a4559b7c7ce7e7affe799961
twstoly <twstoly@gmail.com>
2022-03-30 10:10:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/opticadispatch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.opticadispatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
